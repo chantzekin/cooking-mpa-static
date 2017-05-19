4176328b6eb51135945e1967afde1bd7ad61d8d0
naraku777 <442815900@qq.com>
2017-05-19 18:56:30 +0800

    Add comments

--- cooking.conf.js
+++ cooking.conf.js
@@ -3,53 +3,60 @@
 var path = require('path')
 var cooking = require('cooking')
 
+// app 配置
 var app = require('./app.json')
 
-var entries = function () {
-  var arr = {}
-  app.pages.forEach(function (page) {
-    arr[page.entry] = path.resolve(app.basePath, page.entry)
-  })
-  return arr
-}
+// 读取配置生成入口与模板
+var entries = require('./build/entries')(app)
+var templates = require('./build/templates')(app)
 
 cooking.set({
-  entry: entries(),
+  // 入口文件
+  // entries 模块获取多入口配置
+  entry: entries,
+  // 项目输出路径
   dist: './dist',
-  template: [
-    {
-      filename: 'home.html',
-      template: 'src/pages/home/index.tpl',
-      chunks: ['home']
-    },
-    {
-      filename: 'about.html',
-      template: 'src/pages/about/index.tpl',
-      chunks: ['about']
-    },
-  ],
+  // HTML 模板文件
+  // templates 模块获取多页面模板配置
+  // html-webpack-plugin 指定模板传参
+  template: templates,
 
-  //dev
+  // 开发环境配置
   devServer: {
+    // 是否启用
     enable: true,
+    // 输入文件目录
     publicPath: '/',
+    // 热替换
     hot: true,
+    // 提取 css 到文件
+    // 开发环境，不作提取便于热替换实时改变页面样式
     extractCSS: false,
     port: 8888,
     hostname: 'localhost',
   },
 
-  //prod
+  // 生产环境配置
+  // 构建时先清理输出目录
   clean: true,
+  // 输出文件带 7位 hash，便于管理浏览器文件版本缓存
   hash: true,
   sourceMap: true,
   extractCSS: true,
+  chunk: true,
 
   resolve: {
+    // import 模块可忽略后缀名
     extensions: ['.js', '.json'],
   },
+  alias: {
+    'src': path.join(__dirname, 'src'),
+    '@': path.join(__dirname, 'src', 'assets')
+  },
 
-  extends: ['sass', 'autoprefixer']
+  // 插件
+  // sass: scss文件预编译; autoprefixer: 浏览器兼容样式
+  extends: ['sass']
 })
 
 module.exports = cooking.resolve()
